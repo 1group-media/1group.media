131e65ffa8950c87c4aa57219a31877508a3a16d
Ron Alonzo <alonzo.ronald@gmail.com>
2026-09-20 15:37:30 -0400

    fix(sw): add service worker cache evictor and no-cache headers

---
+++ sw.js
@@ -0,0 +1,16 @@
+// 1group Media Service Worker Cleanup & Cache Eviction
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({ type: 'window' }))
+      .then(clients => {
+        clients.forEach(client => client.navigate(client.url));
+      })
+  );
+});
